464e92b6442666a6857272ed723e1e44cb89d75d
znnznn <znnznn@ukr.net>
2020-04-17 21:07:37 +0300

    лишні

--- 7_firs or last letter.py
+++
@@ -1,8 +0,0 @@
-x = input('Введіть данні: ')
-x_len = len(x)
-if x_len >= 2:
-    x_print = x[0] + x[1] + x[-2] + x[-1]
-    print(x_print)
-else:
-    print()
-
--- 9_check name.py
+++
@@ -1,8 +0,0 @@
-#
-my_name = 'bohdan'
-name = input()
-if my_name.upper() == name.upper():
-    print('Так це моє ім\'я')
-else:
-    print('Ні це не моє ім\'я ')
-
