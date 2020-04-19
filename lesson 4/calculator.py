7508a9d172f93785092750019355ec52d433fddb
znnznn <znnznn@ukr.net>
2020-04-19 20:18:28 +0300

    калькулятор + факторіал + округлення + число кореня у степені

--- lesson 4/calculator.py
+++ lesson 4/calculator.py
@@ -18,6 +18,7 @@ print(name_user_up + """ будь-ласка оберіть операцію, н
 // ділення націло
 % остача від цілочисленого ділення 
 ** підведення числа у степінь
+^^ знайти корінь цілого числа
 ! факторіал
 0 вийти з програми""")
 while True:
@@ -41,14 +42,34 @@ while True:
             print(result_print, factorial)
         else:
             print(f'{name_user_up} {z} не є числом.')
+    elif choice_operation == '^^':
+        digit_root = input(name_user_up + ' введіть число: ')
+        root = input(name_user_up + ' введіть степінь кореня: ')
+        if digit_root.isdigit() and root.isdigit():
+            result_root = float(digit_root) ** (1 / float(root))
+            print(result_print, result_root)
+        else:
+            print(f'{name_user_up} {digit_root} або {root} не є числом.')
     elif choice_operation in ('+', '-', '*', '/', '//', '%', '**', '0'):
         if choice_operation == '0':
             print(f'Дякуємо {name_user_up} . До наступних обчислень.')
             break
         x = input(first_digit)
         y = input(second_digit)
+        x_find_plus = x.find('+')
+        x_find_minus = x.find('-')
+        y_find_plus = y.find('+')
+        y_find_minus = y.find('-')
+        digit_plus_or_minus = max([x_find_plus, x_find_minus, y_find_plus, y_find_minus])
         digit = x + y
-        if not (digit.islower() or digit.isupper()):
+        x_symbol = x.find('/')
+        x_symbol1 = x.find('*')
+        x_symbol2 = x.find(',')
+        y_symbol = y.find('/')
+        y_symbol1 = y.find('*')
+        y_symbol2 = y.find(',')
+        digit_find_symbol = max([x_symbol, x_symbol1, x_symbol2, y_symbol, y_symbol1, y_symbol2])
+        if digit_plus_or_minus <= 0 >= digit_find_symbol and not (digit.islower() or digit.isupper()):
             if choice_operation == '+':
                 print(f'{result_print} {x} + {y} = {(float(x) + float(y)):.{n}f}')
             elif choice_operation == '-':
