5620ebc3199096af0d996a1a5f12f2a8cfc4bfc4
znnznn <znnznn@ukr.net>
2020-04-24 22:12:00 +0300

    список чисел які діляться на сім + кратне п'яти

--- lesson 5/список з чисел  які діляться на сім.py
+++ lesson 5/список з чисел  які діляться на сім.py
@@ -1,12 +1,11 @@
 i = 0
-x = 2
+x = 1
 list_work = []
 list_seven = []
 while i < 100:
     i += 1
     list_work.append(i)
 print(list_work)
-print(list_work.index(7))
 print(len(list_work))
 len_work = len(list_work)
 while x < len_work:
@@ -16,4 +15,3 @@ while x < len_work:
         list_seven.append(list_work.index(x))
     x += 1
 print(list_seven)
-
