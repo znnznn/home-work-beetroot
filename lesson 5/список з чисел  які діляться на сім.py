42afb1062777f374594e49c561b38434e3dd4c1c
znnznn <znnznn@ukr.net>
2020-04-26 14:00:33 +0300

    список чисел які діляться на сім + кратне п'яти (переробив із застосу…  …ванням for in i range)

--- lesson 5/список з чисел  які діляться на сім.py
+++ lesson 5/список з чисел  які діляться на сім.py
@@ -1,9 +1,6 @@
-i = 0
-list_work = []
+ii = 0
+list_work = list(range(1, 101))
 list_seven = []
-while i < 100:
-    i += 1
-    list_work.append(i)
 print(list_work)
 for list_seven in list_work:
     if list_seven % 7 == 0 and not list_seven % 5:
