24719c8f8c7af9318cfd9199c4b99dd45a98f8ea
znnznn <znnznn@ukr.net>
2020-05-04 21:32:30 +0300

    налаштовую імпорт з двух файлів і функція в функції

--- lesson 4/2_auto_mod.py
+++ lesson 4/2_auto_mod.py
@@ -5,15 +5,15 @@ import math
 def check_valid_digit(str_digit):
     symbol_valid = '*+-1234567890./^^'
     symbol_valid_place = str_digit.find('/') * str_digit.find('*') * str_digit.find('^')
-    count_point = str_digit.count('.')
     for item in str_digit:
-        if item not in symbol_valid or symbol_valid_place == 0 or count_point > 2:
+        if item not in symbol_valid or symbol_valid_place == 0:
             return False
         return True
 
 
 def auto_mod(name_user_up):
     while True:
+        print(f'{name_user_up} Для завершення обчислень натисніть 0 :')
         valid_mult = '*/^'
         valid_plus = '-+'
         valid_all = ['*', '+', '-', '/', '^^']
@@ -29,7 +29,7 @@ def auto_mod(name_user_up):
         u = 0
         string_valid = check_valid_digit(w)
         if w == '0':
-            print(f'{name_user_up} До побачення до наступних обчислень.')
+            print(f'{name_user_up} Ви вийшли з автомода.')
             break
         elif string_valid is False:
             print(f'{name_user_up} Ви ввели вираз який не підтримується :')
@@ -39,6 +39,7 @@ def auto_mod(name_user_up):
                 if i in valid_mult and u == 0:   # {'*', '**', '/', '//', '^^'} щоб не було плюса
                         n = w.rpartition(i)
                         u += 1
+                        print(n)
                 if i in valid_plus and u == 0 and 0 != w.find('-'):   # {'+', '-'} окремо бо плюс крутіший
                         n = w.partition(i)
                         u += 1
@@ -46,10 +47,13 @@ def auto_mod(name_user_up):
                         n = w.rpartition(i)
                 if i in valid_all:
                     count = w.count(i)
+                    print(count)
             if n is not False:              # треба щоб відкидало якщо немає другого числа
                 one_digit = n[0]
                 two_digit = n[-1]
                 operation = n[1]
+                print(one_digit, two_digit, operation)
+                # l = eval(w) випадково прочитав таку функцію крутяцьку
                 if count <= 3 and ('-' == one_digit[-1] or one_digit[-1] == '+'):
                     one_digit_new = one_digit[:-1]
                     two_digit_new = one_digit[-1] + two_digit
@@ -76,10 +80,8 @@ def auto_mod(name_user_up):
                     print(f'{name_user_up} Ви ввели незрозумілий вираз :')
                     continue
                 x, y, choice_operation = float(one_digit_new), float(two_digit_new), operation_new
-                print(x, y, choice_operation, 5)
+                print(x, y, choice_operation)
+                from .2_функция import calculator
+                print(calculator(x, y, choice_operation, name_user_up))
             else:
                 print(f'{name_user_up} Ви ввели надто мало даних :')
-        return x, y, choice_operation  # пише Перед призначенням може бути посилання на локальну змінну 'x'
-
-
-print(auto_mod('HHh'))
---
+++ lesson 4/3_функція калькулятора для автомода.py
@@ -0,0 +1,33 @@
+def calculator(x, y, choice_operation, name_user_up):
+    result_print = name_user_up + ' ваш результат:'
+    from  import round
+    n = round()
+    if choice_operation == '^^':
+        digit_root = x
+        root = y
+        if digit_root.isdigit() and root.isdigit():
+            result = float(digit_root) ** (1 / float(root))
+            print(f'{name_user_up}, {result}')
+        else:
+            print(f'{name_user_up} {digit_root} або {root} не є числом.')
+    if choice_operation == '+':
+        print(f'{result_print} {x} + {y} = {(float ( x ) + float ( y )):.{n}f}')
+    elif choice_operation == '-':
+        print(f'{result_print} {x} - {y} = {(float ( x ) - float ( y )):.{n}f}')
+    elif choice_operation == '/':
+        if y == 0:
+            print(f'O o o o p s {name_user_up} ділення на ноль')
+        else:
+            print(f'{result_print} {x} / {y} = {(float ( x ) / float ( y )):.{n}f}')
+    elif choice_operation == '//':
+        if y == 0:
+            print(f'O o o o p s {name_user_up} ділення на ноль')
+        else:
+            print(f'{result_print} {x} // {y} = {float ( x ) // float ( y )}')
+    elif choice_operation == '%':
+        print(f'{result_print} {x} % {y} = {float ( x ) % float ( y )}')
+    elif choice_operation == '*':
+        print(f'{result_print} {x} * {y} = {(float ( x ) * float ( y )):.{n}f}')
+    elif choice_operation == '**':
+        print(f'{result_print} {x} ** {y} = {(float ( x ) ** float ( y )):.{n}f}')
+    return
