2dc728417360c22d3ece50776ef882ee96cc9359
znnznn <znnznn@ukr.net>
2020-05-08 22:00:56 +0300

    функція для роботи з результатлом автомода, калькуоятор для автомода

--- calc_mod/func_calc.py
+++ calc_mod/func_calc.py
@@ -5,11 +5,8 @@ def calculator(x, y, choice_operation, name_user_up):
     if choice_operation == '^^':
         digit_root = x
         root = y
-        if digit_root.isdigit() and root.isdigit():
-            result = float(digit_root) ** (1 / float(root))
-            print(f'{name_user_up}, {result}')
-        else:
-            print(f'{name_user_up} {digit_root} або {root} не є числом.')
+        result = float(digit_root) ** (1 / float(root))
+        print(f'{name_user_up}, {result}')
     if choice_operation == '+':
         print(f'{result_print} {x} + {y} = {(float ( x ) + float ( y )):.{n}f}')
     elif choice_operation == '-':
