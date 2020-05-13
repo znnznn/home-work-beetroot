e66b984f1c69ceaa0c9ab9384823acd708667a49
znnznn <znnznn@ukr.net>
2020-05-13 17:54:32 +0300

    теоефонна книга але ще дві функції сирі це del i edit, але основа працює

--- Lesson_9/phone_book.py
+++ Lesson_9/phone_book.py
@@ -1,28 +1,44 @@
 import json
-my_phone_book = open('phone_book.json', 'w+')
 
-contacts = [
-    {
-        "name": "Mark",
-        "last name": "Markin",
-        "phone": "123456",
-        "city": "Rivne"
-    },
-    {
-        "name": "Julia",
-        "last name": "Markin",
-        "phone": "564321",
-        "city": "Rivne"
-    },
-    {
-        "name": "Bohdan",
-        "last name": "Markin",
-        "phone": "+1234",
-        "city": "Dnipro"
-    },
-]
-with open('phone_book.json', 'w') as phone_book:
-    json.dump(contacts, phone_book, indent=4)
+import sys
+
+try:
+    if len(sys.argv) < 2:
+        print('Введіть базу данних з якою Ви бажаєте працювати')
+        sys_argv1 = input('> ')
+        filename = sys_argv1
+    else:
+        filename = sys.argv[1]
+    json_file = open(filename)
+    print(f'Ви починаєте працювати з {filename} базою даних.')
+except FileNotFoundError:
+    print(f'{sys_argv1} базу даних не знайдено.')
+    filename = 'phone_book.json'
+    print(f'Створити порожню базу данних {filename}? Якщо так натисніть 1.')
+    empty_file_user = input('> ')
+    if empty_file_user == '1':
+        json_file = open(filename, 'w')
+    else:
+        print('До побачення.')
+        exit()
+except Exception:
+    print('До побачення.')
+
+
+try:
+    my_phone_book = json.load(json_file)
+    empty_list = True
+except Exception:
+    empty_list = False
+    my_phone_book = []
+json_file.close()
+
+
+#my_phone_book = open('phone_book.json', 'w+')
+
+
+'''with open('phone_book.json', 'w') as phone_book:
+    json.dump(contacts, phone_book, indent=4)'''
 
 
 
@@ -40,26 +56,27 @@ def choice_find(x, y):
             return t
 
 
-def find(my_phone_book):
-    name = input("> ")
+def find(my_phone_book, criterion):
+    find_value = input("> ")
+    work_my_phone_book = my_phone_book.copy()
     row = []
     item = 0
-    for value in json.load(my_phone_book):
-        for i, x in value.items():
-            if name == x:
-                row.append(value)
-                item += 1
-            else:
-                continue
+    for value in work_my_phone_book:
+        if find_value == value[criterion]:
+            row.append(value)
+            item += 1
+        else:
+            continue
         if item == 0:
-            return print(f'Данні по {i} {name} відсутні.')
-    return print(f"Данні по {i} {name} '\n' {[s for s in row]}', '\n'"), row
+            return print(f'Данні по {criterion} {find_value} відсутні.')
+    return print(f"Данні по {criterion} {find_value}: знайдено {len(row)}'\n' {row}', '\n'"), row
+
 
 
 
 def delete(my_phone_book):
     print("Введіть контакт: ")
-    name = input('> ').strip()
+    name = input('> ').strip().capitalize()
     for value in json.load(my_phone_book):
         if value == name:
             print(f"Ви дійсно бажаєте видалити контект (yes/no)?: {name}")
@@ -76,19 +93,40 @@ def add():
     last_name = input("> ").strip()
     print("Введіть номер телефону")
     phone = input("> ").strip()
+    while not phone.isdigit():
+        print(f'{phone} містить не лише цифри')
+        print("Введіть знову номер телефону")
+        phone = input("> ").strip()
     print("Введіть місто проживання")
     city_live = input("> ").strip()
+    valid_simbol = name + last_name + city_live
+    while not valid_simbol.isalpha() or not phone.isdigit():
+        if not valid_simbol.isalpha() and phone.isdigit():
+            print(f'{name} або {last_name} або {city_live} містять не лише букви. Введіть лише букви')
+            print("Введіть ім\'я контакту")
+            name = input("> ").strip()
+            print("Введіть призвіще контакту")
+            last_name = input("> ").strip()
+            print("Введіть місто проживання")
+            city_live = input("> ").strip()
     new_contact = {
         'name': name,
-        'Last name': last_name,
+        'last name': last_name,
         'phone': phone,
         'city': city_live
     }
-
-    with open('phone_book.json', 'a') as phone_book:
-        json.dump(new_contact, phone_book)
-        contacts.append(new_contact)
+    phone_book = open(filename, 'r+')
+    try:
+        all_contacts = json.load(phone_book)
+        all_contacts.append(new_contact)
+    except json.decoder.JSONDecodeError:
+        all_contacts = []
+        all_contacts.append(new_contact)
+        my_phone_book.append(new_contact)
+    phone_book.seek(0)
+    json.dump(all_contacts, phone_book, indent=4)
     print('Контакт збережено')
+    phone_book.close()
 
 
 find_list = {
@@ -107,43 +145,54 @@ operation_list = {
     'exit': 'вихід'
 }
 operation_listToprint = [': '.join(prety) for prety in operation_list.items()]
+print(f'В телефонній книзі доступні наступні команди.')
 print(*operation_listToprint, sep='\n')
-
-while True:
-    print("Введiть команду: ")
-    command1 = input('> ').strip()
-    command = {''.join(command1)}
-    command = choice_find(command, operation_list)
-    if command == 'list':
-        list(my_phone_book)
-    elif command == 'find':
-        print(*find_listToprint, sep='\n')
-        print("\nОберіть критерій пошуку: ")
-        command_find = input('>').strip()
-        command_find = {''.join(command_find)}
-        command_find = choice_find(command_find, find_list)
-        if command_find == 'name':
-            print("Введіть iм'я: ")
-            print(find(my_phone_book))
-        elif command_find == 'last name':
-            print("Введіть призвіще: ")
-            find(my_phone_book)
-        elif command_find == 'city':
-            print("Введіть місто проживання: ")
-            find(my_phone_book)
-        elif command_find == 'find phone':
-            print("Введіть номер телеіону: ")
-            find(my_phone_book)
+try:
+    while True:
+        print("Введiть команду: ")
+        command1 = input('> ').strip().lower()
+        command = {''.join(command1)}
+        command = choice_find(command, operation_list)
+        if command == 'exit':
+            break
+        elif command == 'add':
+            add()
+            empty_list = True
+        elif empty_list is True:
+            if command == 'list':
+                list ( my_phone_book )
+            elif command == 'del':
+                delete(my_phone_book)
+            elif command == 'find':
+                print(*find_listToprint, sep='\n')
+                print("\nОберіть критерій пошуку: ")
+                command_find = input('>').strip()
+                command_find = {''.join(command_find)}
+                command_find = choice_find(command_find, find_list)
+                if command_find == 'name':
+                    criterion = 'name'
+                    print("Введіть iм'я: ")
+                    print(find(my_phone_book, criterion))
+                elif command_find == 'last name':
+                    criterion = 'last name'
+                    print("Введіть призвіще: ")
+                    find(my_phone_book, criterion)
+                elif command_find == 'city':
+                    print("Введіть місто проживання: ")
+                    criterion = 'city'
+                    find(my_phone_book, criterion)
+                elif command_find == 'phone':
+                    criterion = 'phone'
+                    print("Введіть номер телеіону: ")
+                    find(my_phone_book, criterion)
+                else:
+                    print("Даний критерій пошуку не підтримується")
+        elif empty_list is False and command is not None:
+            print('База даних порожня тому пошук, видалення, редагування неможливий. Спробуйте іншу команду.')
+            '''elif command == 'edit':
+            edit(my_phone_book)'''
         else:
-            print("Даний критерій пошуку не підтримується")
-    elif command == 'add':
-        add()
-    elif command == 'del':
-        delete(my_phone_book)
-        '''elif command == 'edit':
-        edit(my_phone_book)'''
-    elif command == 'exit':
-        break
-    else :
-        print(f"{command1} ця команда не підтримується.")
-my_phone_book.close()
+            print(f"{command1} ця команда не підтримується.")
+#exept
+finally:
+    json_file.close()
