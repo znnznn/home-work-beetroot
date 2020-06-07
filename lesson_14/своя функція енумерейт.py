5c6c4c598cad872eee3ff0deab7e0d7567e0a20d
znnznn <znnznn@ukr.net>
2020-06-08 00:20:37 +0300

    своя реалізація функції enumerate + функції range + поміняв місцями в for індекс і значення

--- lesson_14/своя функція енумерейт.py
+++ lesson_14/своя функція енумерейт.py
@@ -15,9 +15,40 @@ def in_range(start, end, step=1):
         numder += step
     return my_range
 
-proba = 'Mamba'
 
+class My_iterable:
+
+    def __init__(self, *args):
+        self.my_iterable = args
+        self.index = 0
+
+    def __str__(self):
+        return f'{self.my_iterable}'
+
+    def __repr__(self):
+        return f'{self.my_iterable}'
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.my_iterable):
+            raise StopIteration
+        item_iterable = list(self.my_iterable[self.index])
+        self.index += 1
+        return item_iterable, self.index - 1
+
+
+proba = 'Mamba'
+newProba = ['go', 1, 2, 'terra']
+newProba1 = ['lats', 3, 4, 'mars']
+proba1 = My_iterable(newProba, newProba1)
 result = with_index(proba)
 print(result)
 result_range = in_range(0, 15, 5)
 print(result_range)
+print(proba1)
+#print(next(proba1))
+for item, i in proba1:
+    print(item)
+    print(i)
