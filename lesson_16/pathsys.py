e463c0fa2a736f400b08823cfcc461adc2a4a1a7
znnznn <znnznn@ukr.net>
2020-06-11 00:13:39 +0300

    рщбота з sys.path

---
+++ lesson_16/pathsys.py
@@ -0,0 +1,9 @@
+import sys
+print(sys.path)
+print()
+sys.path.append("vlasyuk\\AppData\\Local\\Programs\\Python\\Python38-32\\lib\\path\\to\\my\\module")
+print(sys.path)
+print()
+sys.path.remove("vlasyuk\\AppData\\Local\\Programs\\Python\\Python38-32\\lib\\path\\to\\my\\module")
+print(sys.path)
+print()
