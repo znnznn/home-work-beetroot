e04907273078f409cf05e7fbe3cf4ba2ba9650f8
znnznn <znnznn@ukr.net>
2020-06-21 17:17:24 +0300

    тест функції на валідність операції

--- lesson_18/test_phone.py
+++ lesson_18/test_phone.py
@@ -1,17 +1,16 @@
 import unittest
-#from lesson_17 import typy_in_func
+#  from lesson_17.typy_in_func import choice_find
+from lesson_17.typy_in_func import choice_find
 
-
-def choice_find(x, y):
+'''def choice_find(x, y):    
     """"checking operation valid"""
     print(x, y)
     choice_oper = None
     for t in y:
-        if t not in x:
-            return choice_oper
-        else:
+        if t == x:
             choice_oper = t
-            return choice_oper
+        return choice_oper
+'''
 
 
 class MyTestPhoneBook(unittest.TestCase):
@@ -19,26 +18,26 @@ class MyTestPhoneBook(unittest.TestCase):
     def test_choice_find(self):
         """"function test checking operation valid with dict"""
 
-        result_choice_find = choice_find('yyy', {'mylist': 'ok'})
+        result_choice_find = choice_find('yyy', {'list': 'ok'})
         self.assertEqual(result_choice_find, None)
 
     def test_choice_find_not_none(self):
         """"function test checking operation valid with dict """
 
-        result_choice_find = choice_find('mylist', {'mylist': 'ok'})
-        self.assertEqual(result_choice_find, 'mylist')
+        result_choice_find = choice_find('list', {'list': 'ok'})
+        self.assertEqual(result_choice_find, 'list')
 
     def test_choice_find_empty_data(self):
         """"function test checking operation valid with empty list """
 
-        result_choice_find = choice_find('mylist', [])
+        result_choice_find = choice_find('list', [])
         self.assertEqual(result_choice_find, None)
 
     def test_choice_find_list(self):
         """"function test checking operation valid with list """
 
-        result_choice_find = choice_find('mylist', ['mylist'])
-        self.assertEqual(result_choice_find, 'mylist')
+        result_choice_find = choice_find('list', ['list', 1])
+        self.assertEqual(result_choice_find, 'list')
 
 
 if __name__ == '__main__':
