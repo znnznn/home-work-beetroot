23b3d0f0e7047a3c313c945c8415f7077ad112a3
znnznn <znnznn@ukr.net>
2020-06-21 17:18:08 +0300

    вказування очикуваних типів значень в функціях

--- lesson_17/typy_in_func.py
+++ lesson_17/typy_in_func.py
@@ -2,39 +2,8 @@ import json
 import sys
 
 
+
 from typing import Dict, Type, Union, Optional, Any, Iterable, Iterator, List
-
-try:
-    if len(sys.argv) < 2:
-        print('Введіть базу данних з якою Ви бажаєте працювати')
-        sys_argv1 = input('> ')
-        filename = sys_argv1
-    else:
-        filename = sys.argv[1]
-    print(sys.argv)
-    json_file = open(filename, 'r')
-    print(f'Ви починаєте працювати з {filename} базою даних.')
-except FileNotFoundError:
-    print(f'{sys_argv1} базу даних не знайдено.')
-    filename = 'phone_book.json'
-    print(f'Створити порожню базу данних {filename}? Якщо так натисніть 1.')
-    empty_file_user = input('> ')
-    if empty_file_user == '1':
-        json_file = open(filename, 'w')
-    else:
-        print('До побачення.')
-        exit()
-except Exception:
-    print('Ви створили критичну помилку. Перезапустіть програму.')
-    exit()
-
-try:
-    my_phone_book = json.load(json_file)
-    empty_list = True
-except Exception:
-    empty_list = False
-    my_phone_book = []
-json_file.close()
 
 
 def list(filename: str):
@@ -47,15 +16,13 @@ def choice_find(x: Any, y: Dict) -> Optional[Dict]:
     """"checking operation valid"""
     print(x, y)
     choice_oper = None
-    for t in x:
-        if t not in y:
-            return choice_oper
-        else:
+    for t in y:
+        if t == x:
             choice_oper = t
-            return choice_oper
+        return choice_oper
 
 
-def find(my_phone_book: List, criterion: str) -> Optional[str, List, int]:
+def find(my_phone_book: list, criterion: str) -> Union[str, list]:
     """"search for an item by criterion"""
     find_value = input("> ").strip().capitalize()
     work_my_phone_book = my_phone_book.copy()
@@ -189,80 +156,116 @@ operation_list = {
 }
 
 operation_listToprint = [': '.join(pretty) for pretty in operation_list.items()]
-print(f'В телефонній книзі {filename} доступні наступні команди.')
-print(*operation_listToprint, sep='\n')
 
-try:
-    while True:
-        print("Введiть команду: ")
-        command1 = input('> ').strip().lower()
-        command = {''.join(command1)}
-        print(command)
-        command = choice_find(command, operation_list)
-        if command == 'exit':
-            break
-        elif command == 'add':
-            add()
-            empty_list = True
-        elif empty_list is True:
-            if command == 'list':
-                list(filename)
-            elif command == 'edit':
-                print(*find_listToprint, sep='\n')
-                print('Оберіть критерій пошуку')
-                command_find1 = input('>').strip()
-                command_find = {''.join(command_find1)}
-                command_find = choice_find(command_find, find_list)
-                if command_find is not None:
-                    command_find = command_find1
-                    print('Введіть пошукове значення')
-                    edit_find, index_edit = find(my_phone_book, command_find)
-                    if len(index_edit) != 0:
-                        if len(index_edit) > 1:
-                            print(index_edit, len(index_edit))
-                            print('Ви вибрали для зміни більше ніж один контакт. Спробуйте ще раз')
-                            continue
-                        else:
-                            print(index_edit)
-                            edit(index_edit)
-                    else:
-                        print('Даного контакту не існує. Спробуйте ще раз.')
-                        continue
-                else:
-                    print("Даний критерій пошуку не підтримується")
-            elif command == 'del':
-                criterion = 'phone'
-                delete(criterion)
-            elif command == 'find':
-                print(*find_listToprint, sep='\n')
-                print("\nОберіть критерій пошуку: ")
-                command_find = input('>').strip()
-                command_find = {''.join(command_find)}
-                command_find = choice_find(command_find, find_list)
-                if command_find == 'name':
-                    criterion = 'name'
-                    print("Введіть iм\'я: ")
-                    find(my_phone_book, criterion)
-                elif command_find == 'last name':
-                    criterion = 'last name'
-                    print("Введіть призвіще: ")
-                    find(my_phone_book, criterion)
-                elif command_find == 'city':
-                    print("Введіть місто проживання: ")
-                    criterion = 'city'
-                    find(my_phone_book, criterion)
-                elif command_find == 'phone':
-                    criterion = 'phone'
-                    print("Введіть номер телефону: ")
-                    find(my_phone_book, criterion)
-                else:
-                    print("Даний критерій пошуку не підтримується")
-        elif empty_list is False and command is not None:
-            print('База даних порожня тому пошук, видалення, редагування неможливий. Спробуйте іншу команду.')
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) < 2:
+            print('Введіть базу данних з якою Ви бажаєте працювати')
+            sys_argv1 = input('> ')
+            filename = sys_argv1
         else:
-            print(f"{command1} ця команда не підтримується.")
-except Exception:
-    print('Вибачте сталась критична помилка. Вам доведеться перезапустити програму.')
-finally:
-    print('Дані збережено у файл.')
+            filename = sys.argv[1]
+        print(sys.argv)
+        json_file = open(filename, 'r')
+        print(f'Ви починаєте працювати з {filename} базою даних.')
+    except FileNotFoundError:
+        print(f'{sys_argv1} базу даних не знайдено.')
+        filename = 'phone_book.json'
+        print(f'Створити порожню базу данних {filename}? Якщо так натисніть 1.')
+        empty_file_user = input('> ')
+        if empty_file_user == '1':
+            json_file = open(filename, 'w')
+        else:
+            print('До побачення.')
+            exit()
+    except Exception:
+        print('Ви створили критичну помилку. Перезапустіть програму.')
+        exit()
+
+    try:
+        my_phone_book = json.load(json_file)
+        empty_list = True
+    except Exception:
+        empty_list = False
+        my_phone_book = []
     json_file.close()
+    print(f'В телефонній книзі {filename} доступні наступні команди.')
+    print(*operation_listToprint, sep='\n')
+
+    try:
+        while True:
+            print("Введiть команду: ")
+            command1 = input('> ').strip().lower()
+            command = {''.join(command1)}
+            print(command)
+            command = choice_find(command, operation_list)
+            if command == 'exit':
+                break
+            elif command == 'add':
+                add()
+                empty_list = True
+            elif empty_list is True:
+                if command == 'list':
+                    list(filename)
+                elif command == 'edit':
+                    print(*find_listToprint, sep='\n')
+                    print('Оберіть критерій пошуку')
+                    command_find1 = input('>').strip()
+                    command_find = {''.join(command_find1)}
+                    command_find = choice_find(command_find, find_list)
+                    if command_find is not None:
+                        command_find = command_find1
+                        print('Введіть пошукове значення')
+                        edit_find, index_edit = find(my_phone_book, command_find)
+                        if len(index_edit) != 0:
+                            if len(index_edit) > 1:
+                                print(index_edit, len(index_edit))
+                                print('Ви вибрали для зміни більше ніж один контакт. Спробуйте ще раз')
+                                continue
+                            else:
+                                print(index_edit)
+                                edit(index_edit)
+                        else:
+                            print('Даного контакту не існує. Спробуйте ще раз.')
+                            continue
+                    else:
+                        print("Даний критерій пошуку не підтримується")
+                elif command == 'del':
+                    criterion = 'phone'
+                    delete(criterion)
+                elif command == 'find':
+                    print(*find_listToprint, sep='\n')
+                    print("\nОберіть критерій пошуку: ")
+                    command_find = input('>').strip()
+                    command_find = {''.join(command_find)}
+                    command_find = choice_find(command_find, find_list)
+                    if command_find == 'name':
+                        criterion = 'name'
+                        print("Введіть iм\'я: ")
+                        find(my_phone_book, criterion)
+                    elif command_find == 'last name':
+                        criterion = 'last name'
+                        print("Введіть призвіще: ")
+                        find(my_phone_book, criterion)
+                    elif command_find == 'city':
+                        print("Введіть місто проживання: ")
+                        criterion = 'city'
+                        find(my_phone_book, criterion)
+                    elif command_find == 'phone':
+                        criterion = 'phone'
+                        print("Введіть номер телефону: ")
+                        find(my_phone_book, criterion)
+                    else:
+                        print("Даний критерій пошуку не підтримується")
+            elif empty_list is False and command is not None:
+                print('База даних порожня тому пошук, видалення, редагування неможливий. Спробуйте іншу команду.')
+            else:
+                print(f"{command1} ця команда не підтримується.")
+    except Exception:
+        print('Вибачте сталась критична помилка. Вам доведеться перезапустити програму.')
+    finally:
+        print('Дані збережено у файл.')
+        json_file.close()
+
+
