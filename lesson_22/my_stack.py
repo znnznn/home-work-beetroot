11c6acea004106d741502a80466ccb4a11b0435e
znnznn <znnznn@ukr.net>
2020-07-08 22:54:34 +0300

    my_stack

--- lesson_22/my_stack.py
+++ lesson_22/my_stack.py
@@ -5,7 +5,7 @@ class My_Stack:
         self.cool_stack: List[Any] = []
 
     def empty_cool_stack(self) -> bool:
-        if len(self.cool_stack) == 0:
+        if len(list(self.cool_stack)) == 0:
             return True
         return False
 
@@ -13,30 +13,31 @@ class My_Stack:
         self.cool_stack.append(add_item)
 
     def take_cool_stack(self) -> Any:
-        if len(self.cool_stack) == 0:
-            return
-        return self.cool_stack.pop()
+        if len(self.cool_stack) != 0:
+            return self.cool_stack.pop()
 
     def __str__(self):
         return f'{list(self.cool_stack)}'
+
 rrr = [i for i in range(10)]
 super_stack = My_Stack()
 new_super_stack = My_Stack()
-print(rrr)
-print ( super_stack, 'lll;' )
+
+print ( super_stack, '0' )
 x = 0
-while x == 0:
-    if super_stack.empty_cool_stack == True:
-        x = 1
-        for takes in super_stack.take_cool_stack():
-            new_super_stack.add_cool_stack(takes)
 
-        print ( super_stack, 'uuu' )
-    elif super_stack.empty_cool_stack() == True:
-
-        for adds in rrr:
-            super_stack.add_cool_stack(adds)
-        print ( super_stack, 'ppp' )
-print(super_stack.empty_cool_stack(), '888')
-print(super_stack, '555')
-print(new_super_stack, '222')
+if super_stack.empty_cool_stack is False:
+    for takes in super_stack.take_cool_stack():
+        new_super_stack.add_cool_stack(takes)
+if super_stack.empty_cool_stack() is True:
+    for adds in rrr:
+        super_stack.add_cool_stack(adds)
+print(super_stack, '1')
+print(super_stack.empty_cool_stack(), '2')
+while super_stack.empty_cool_stack() is False:
+    new_super_stack.add_cool_stack(super_stack.take_cool_stack())
+print (new_super_stack, '3')
+print(super_stack.empty_cool_stack(), '4')
+print(super_stack, '5')
+print(new_super_stack, '6')
+print(super_stack.empty_cool_stack())
