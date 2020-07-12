022fc7a14d034839ec3575048a7386a7af4a0308
znnznn <znnznn@ukr.net>
2020-07-13 00:41:28 +0300

    my sockets

--- lesson_25/my_client.py
+++ lesson_25/my_client.py
@@ -2,3 +2,4 @@ import socket
 
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket_client.sendto(b'Hello Word, I am beetroot student >:)', ('127.0.0.1', 65000))
+
--- lesson_25/my_server.py
+++ lesson_25/my_server.py
@@ -5,3 +5,4 @@ server_socket.bind(('127.0.0.1', 65000))
 server_send = server_socket.recv(1024)
 print(server_send)
 server_socket.close()
+
