73bb8a191f366fc9316f98e7e29e2b3962295738
znnznn <znnznn@ukr.net>
2020-07-13 23:22:01 +0300

    my server and my client

--- lesson_25/my_client.py
+++ lesson_25/my_client.py
@@ -1,5 +1,7 @@
 import socket
 
-socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_client.sendto(b'Hello Word, I am beetroot student >:)', ('127.0.0.1', 65000))
+
+def my_client(text):
+    socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_client.sendto(text, ('127.0.0.1', 65000))
 
--- lesson_25/my_server.py
+++ lesson_25/my_server.py
@@ -1,8 +1,90 @@
+import sys
 import socket
+import math
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('127.0.0.1', 65000))
-server_send = server_socket.recv(1024)
-print(server_send)
-server_socket.close()
+
+from functools import partial
+from typing import Union, NoReturn
+
+from PyQt5.Qt import Qt
+
+from PyQt5.QtWidgets import (QApplication,
+                             QComboBox,
+                             QSizePolicy,
+                             QWidget,
+                             QVBoxLayout,
+                             QGridLayout,
+                             QMainWindow,
+                             QPushButton,
+                             QLineEdit,
+                             QLabel)
+
+
+class MyServer(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        """ Creates a server window"""
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle('MyServer')
+        self.widget = QWidget(self.setGeometry(60, 30, 50, 50))
+        #  widget = QWidget(self.setFixedSize(550, 550))
+        self.first_label = QLabel('<h1><b><i>Мікро сервер</i></b></h1>')
+        self.editArea = QLineEdit('')
+        self.editArea.setReadOnly(True)
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.first_label)
+        mainLayout.addWidget(self.editArea)
+        self.second_label = QLabel('')
+        mainLayout.addWidget(self.second_label)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind(('localhost', 65000))
+        #self.server_send = self.server_socket.recv(1024)
+        buttonLayout = QGridLayout()
+        self.button_round = QComboBox()
+        mainLayout.addWidget(self.button_round)
+
+        buttons = [
+            {
+                'name': 'on/off',
+                'row': 0,
+                'col': 0
+            }
+        ]
+        self.buttons = {}
+        for buttonConfig in buttons:
+            name = buttonConfig.get('name', '')
+            btn = QPushButton(name)
+            btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+            self.buttons[name] = btn
+            buttonLayout.addWidget(btn,
+                                   buttonConfig['row'],
+                                   buttonConfig['col'],
+                                   1,
+                                   buttonConfig.get('colSpan', 1))
+        mainLayout.addLayout(buttonLayout)
+        self.widget.setLayout(mainLayout)
+        self.setCentralWidget(self.widget)
+
+        for buttonName in self.buttons:
+            btn = self.buttons[buttonName]
+            if buttonName == 'on/off':
+                btn.clicked.connect(partial(self.start_server))
+
+    def start_server(self):
+        while True:
+            server_send = self.server_socket.recv(1024)
+            self.editArea.setText(server_send)
+            self.server_socket.close()
+            break
+
+def main_window() -> None:
+    """" calculator start function """
+    app = QApplication(sys.argv)
+    window = MyServer()
+    window.show()
+    return_code = app.exec()
+    sys.exit(return_code)
+
+
+if __name__ == '__main__':
+    main_window()
 
