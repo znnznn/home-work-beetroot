c3d706ac304a7966f08b343e0394ff02b7758378
znnznn <znnznn@ukr.net>
2020-07-19 22:07:42 +0300

    weather pqt5 (тайпінг, шрифти, позабирав лишні рядки коду)

--- lesson_26/weather_pqt5.py
+++ lesson_26/weather_pqt5.py
@@ -1,12 +1,10 @@
 import sys
 import requests
 
-from functools import partial
-
+from typing import NoReturn, List, Dict
 from PyQt5.QtWidgets import (QApplication,
                              QSizePolicy,
                              QWidget,
-                             QHBoxLayout,
                              QVBoxLayout,
                              QGridLayout,
                              QMainWindow,
@@ -17,34 +15,32 @@ from PyQt5.QtWidgets import (QApplication,
 
 class MyClient(QMainWindow):
     def __init__(self, *args, **kwargs):
-        """ Creates a server window """
+        """ Creates a weather window """
         super().__init__(*args, **kwargs)
         self.setWindowTitle('MyWeather')
         self.widget = QWidget(self.setGeometry(600, 600, 100, 100))
-        #self.widget = QWidget(self.setFixedSize(550, 250))
-        self.first_label = QLabel('<h1><b><i>Виберіть місто</i></b></h1>')
+        self.first_label = QLabel('<h1><b><i>Виберіть місто / Found city</i></b></h1>')
         self.editArea = QLineEdit('')
-        box = QHBoxLayout()
         self.editArea.setReadOnly(False)
-        mainLayout = QVBoxLayout()
 
+        mainLayout = QVBoxLayout()
         mainLayout.addWidget(self.first_label)
         mainLayout.addWidget(self.editArea)
         self.second_label = QLabel('')
         mainLayout.addWidget(self.second_label)
+
         self.url = 'http://api.openweathermap.org/data/2.5/weather'
         self.key = '5387623c612af64f83da5b790beef122'
 
-
         buttonLayout = QGridLayout()
-        buttons = [
+        buttons: List = [
             {
                 'name': 'Дізнатись погоду',
                 'row': 0,
                 'col': 0
             }
         ]
-        self.buttons = {}
+        self.buttons: Dict = {}
         for buttonConfig in buttons:
             name = buttonConfig.get('name', '')
             btn = QPushButton(name)
@@ -62,32 +58,32 @@ class MyClient(QMainWindow):
         for buttonName in self.buttons:
             btn = self.buttons[buttonName]
             if buttonName == 'Дізнатись погоду':
-                btn.clicked.connect(partial(self.my_weather))
+                btn.clicked.connect(self.my_weather)
 
-    def my_weather(self):
-        """" sends a message to the server  """
+    def my_weather(self) -> NoReturn:
+        """" sends a request for weather in the city on the HTTP server  """
         try:
             if self.editArea.text():
+                self.second_label.clear()
+                self.widget = self.setGeometry(600, 600, 100, 100)
                 city = self.editArea.text()
-
-                weather = requests.get(self.url, params={'q': city, "units": "metric", 'appid': str(self.key)})
+                weather = requests.get(self.url, params={'q': city, 'units': 'metric', 'appid': self.key})
                 weather.raise_for_status()
                 weather_city = weather.json()
-                self.second_label.clear()
-                self.second_label.setText(f"<h4><b><i>{str(weather_city['main']).strip('{').strip('}')}</i></b></h4>")
+                self.second_label.setText(f"<h4><b><i>{str(weather_city['main']).strip('{}')}</i></b></h4>")
             else:
                 raise ValueError
         except ValueError:
-            self.second_label.setText('Оберіть інше місто для прогнозу погоди')
+            self.second_label.setText('<h4><b><i>Оберіть інше місто для прогнозу погоди</i></b></h4>')
         except Exception as e:
             if str(e).count('4') > 1:
-                self.second_label.setText(f'Місто {city.capitalize()} не знайдено')
+                self.second_label.setText(f'<h4><b><i>Місто {city.capitalize()} не знайдено</i></b></h4>')
             else:
                 self.second_label.setText(f'Error: {e}')
 
 
 def main_window() -> None:
-    """" client start function """
+    """" weather request start function """
     app = QApplication(sys.argv)
     window_client = MyClient()
     window_client.show()
@@ -97,6 +93,3 @@ def main_window() -> None:
 
 if __name__ == '__main__':
     main_window()
-
-
-
