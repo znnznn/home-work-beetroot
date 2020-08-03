30308c36ec469de60aa15a9855f914b2d76a0c33
znnznn <znnznn@ukr.net>
2020-08-04 00:50:15 +0300

    зробив через рекурсію. і викорисання вже існуючих фуекцій

--- lesson_28/asincio_factorial.py
+++ lesson_28/asincio_factorial.py
@@ -13,8 +13,16 @@ async def factorials(n):
     return f
 
 
+async def factorials_recurs(n):
+    print(f'start factorial recurs with {n}')
+    if n == 1:
+        return 1
+    f = await factorials_recurs(n-1) * n
+    return f
+
+
 async def fibonacci(n):
-    print(f'start fibonacci numder {n}')
+    print(f'start fibonacci number {n}')
     fp1 = 0
     fn2 = 1
     i = 1
@@ -25,29 +33,29 @@ async def fibonacci(n):
         fp1 = fn2
         fn2 = fib
         i += 1
-
+        await asyncio.sleep(0.5)
     print(f'end fibonacci number {n} with result {fib}')
     return fib
 
 
 async def pow(n):
-    print(f'start pow numder {n}')
-    await asyncio.sleep ( 0.5)
+    print(f'start pow number {n}')
     k = n ** 2
+    await asyncio.sleep(0.5)
     print(f'end pow digit {n} with result {k}')
     return k
 
 
 async def pow_3(n):
-    print(f'start pow numder {n}')
+    print(f'start pow number {n}')
     await asyncio.sleep (0.3)
-    k = n ** 3
+    k = await pow(n) * n
     print(f'end pow_3 digit {n} with result {k}')
     return k
 
 
 async def main1():
     for n in range(1, 11):
-        await asyncio.gather(factorials(n), fibonacci(n), pow(n), pow_3(n))
+        await asyncio.gather(factorials(n), factorials_recurs(n), fibonacci(n), pow(n), pow_3(n))
 
 print(asyncio.run(main1()))
