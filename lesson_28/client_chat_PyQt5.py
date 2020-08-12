fbc4ded45460c2fa18cf40d18217aba488c67533
znnznn <znnznn@ukr.net>
2020-08-13 00:04:37 +0300

    сервер і чат підчистив

--- lesson_28/client_chat_PyQt5.py
+++ lesson_28/client_chat_PyQt5.py
@@ -1,11 +1,12 @@
 import asyncio
 import sys
-import threading
+
 
 from functools import partial
-
+from quamash import QEventLoop
 from PyQt5.QtWidgets import (QApplication,
                              QSizePolicy,
+                             QTextEdit,
                              QWidget,
                              QVBoxLayout,
                              QGridLayout,
@@ -16,28 +17,34 @@ from PyQt5.QtWidgets import (QApplication,
 
 
 class MyClient(QMainWindow):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, loop, *args, **kwargs):
         """ Creates a server window """
         super().__init__(*args, **kwargs)
         self.setWindowTitle('MyClient')
         self.widget = QWidget(self.setGeometry(60, 30, 50, 50))
-        #  widget = QWidget(self.setFixedSize(550, 550))
-        self.first_label = QLabel('<h1><b><i>Мікро клієнт</i></b></h1>')
+
+        self.first_label = QLabel('<h2><b><i>Мікро чат</i></b></h2>')
         self.editArea = QLineEdit('')
-        self.editArea.setReadOnly(False)
         mainLayout = QVBoxLayout()
         mainLayout.addWidget(self.first_label)
-        mainLayout.addWidget(self.editArea)
-        self.second_label = QLabel('')
-        mainLayout.addWidget(self.second_label)
-        self.loop = None
 
-        buttonLayout = QGridLayout()
+        self.loop = loop
+        self.msg = None
+
+        chat_send = QGridLayout()
+        chat_send.setSpacing(2)
+        chat = QGridLayout()
+        chat.setSpacing(25)
+        self.chat = QTextEdit()
+        self.chat.setReadOnly(True)
         btn = QPushButton('send')
         btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
 
-        buttonLayout.addWidget(btn)
-        mainLayout.addLayout(buttonLayout)
+        chat.addWidget(self.chat, 1, 24)
+        chat_send.addWidget(btn, 25, 1)
+        chat_send.addWidget(self.editArea, 25, 0)
+        mainLayout.addLayout(chat)
+        mainLayout.addLayout(chat_send)
         self.widget.setLayout(mainLayout)
         self.setCentralWidget(self.widget)
         btn.clicked.connect(partial(self.my_client))
@@ -45,57 +52,56 @@ class MyClient(QMainWindow):
     def my_client(self):
         """" sends a message to the server  """
         try:
-            send_client = self.editArea.text()
+            self.msg = self.editArea.text()
             self.editArea.clear()
-            asyncio.run(self.main(send_client))
+
         except Exception as e:
             self.second_label.setText(f'Error: {e}')
 
-    async def main(self, text):
-        self.loop = asyncio.get_running_loop()
-
-        on_con_lost = self.loop.create_future()
-        message = text
-
-        transport, protocol = await self.loop.create_connection(
-            lambda: ClientChat(message, on_con_lost),
-            '127.0.0.1', 8888)
-        try:
-            await on_con_lost
-        finally:
-            transport.close()
-
 
 class ClientChat(asyncio.Protocol, MyClient):
-    def __init__(self, message, on_con_lost, *args, **kwargs):
+    def __init__(self, loop, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.message = message
-        self.on_con_lost = on_con_lost
+        self.loop = loop
+        self.message = None
+        self.transport = None
 
     def connection_made(self, transport):
-
-        transport.write(self.message.encode())
+        self.message = self.msg
+        self.transport = transport
+        self.transport.write(self.message.encode())
         print(f'Data sent: {self.message}')
 
     def data_received(self, data):
 
         text = data.decode()
-
-        self.second_label.setText(text)
+        self.chat.setText(text)
         print(f'Data received: {text}')
 
     def connection_lost(self, exc):
-        print('The server closed the connection')
-        self.on_con_lost.set_result(True)
+        self.chat.setText('The server closed the connection')
+
+
+class App(QApplication):
+    def __init__(self):
+        QApplication.__init__(self, sys.argv)
+        self.loop = QEventLoop(self)
+        asyncio.set_event_loop(self.loop)
+
+        self.client = ClientChat(self.loop, self.loop)
+        self.loop.create_task(self.start())
+
+        self.gui = MyClient(self.loop)
+        self.gui.show()
+        self.loop.run_forever()
+
+    async def start(self):
+        connection = self.loop.create_connection(lambda: self.client, '127.0.0.1', 8888)
+        await asyncio.wait_for(connection, 10000)
 
 
 def main_window() -> None:
-    """" client start function """
-    app = QApplication(sys.argv)
-    window_client = MyClient()
-    window_client.show()
-    return_code = app.exec()
-    sys.exit(return_code)
+    App()
 
 
 if __name__ == '__main__':
--- lesson_28/server_chat.py
+++ lesson_28/server_chat.py
@@ -4,52 +4,56 @@ from typing import Any, Optional
 
 class MySeverChat(asyncio.Protocol):
 
-    def __init__(self, loop):
+    def __init__(self, loop, clients):
         self.msg = []
+        self.message = None
+        self.clients = clients
         self.loop = loop
         self.now_client = None
-        self.client = []
         self.peername = None
 
     def connection_lost(self, exc: Optional[Exception]) -> None:
         try:
-            self.client.remove(self.now_client)
+            self.clients.remove(self.now_client)
         except:
-            self.loop.stop()
-        self.loop.stop()
+            pass
 
     def connection_made(self, client) -> None:
         self.peername = client.get_extra_info('peername')
         print(f'connection : {self.peername}')
         self.now_client = client
-        self.client.append(self.now_client)
+        self.clients.append(self.now_client)
         self.send()
 
     def data_received(self, data: bytes) -> None:
-        self.msg.append(f'{self.peername}: {data.decode()}')
+        self.message = f'{self.peername[1]}: {data.decode()}\n'
+        self.msg.append(f'{self.peername[1]}: {data.decode()}\n')
         print(*self.msg)
         self.send()
 
     def send(self):
-        for client in self.client:
-            for msg in self.msg:
-                client.write(bytes(msg, encoding='utf-8'))
+        for client in self.clients:
+            if client == self.now_client:
+                for msg in self.msg:
+                    client.write(bytes(msg, encoding='utf-8'))
+            else:
+                client.write(bytes(self.message, encoding='utf-8'))
 
 
+def main():
+    clients = []
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(lambda: MySeverChat(loop, clients), '127.0.0.1', 8888)
+    server = loop.run_until_complete(coro)
+    print(f'Serving on {(server.sockets[0].getsockname())}')
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
 
 
-loop = asyncio.get_event_loop()
-coro = loop.create_server(lambda: MySeverChat(loop), '127.0.0.1', 8888)
-server = loop.run_until_complete(coro)
-
-
-print(f'Serving on {(server.sockets[0].getsockname())}')
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
-
-# Close the server
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+if __name__ == '__main__':
+    main()
